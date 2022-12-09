54101ee96dc983163f941a5c010d10e8ded91af2
alexandrubites <alexandrubites@gmail.com>
2022-12-09 00:59:24 -0600

    se agrego la rama education y se agrego el codigo #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkexperienceComponent } from './workexperience/workexperience.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkexperienceComponent
+    WorkexperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
